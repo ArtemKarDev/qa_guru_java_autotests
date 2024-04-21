2344f701bce8483549181ff765b7a830e537deef
ArtemKarDev <akm86@mail.ru>
2024-04-21 13:15:49 +0300

    added config remote

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import io.qameta.allure.internal.shadowed.jackson.databind.cfg.ConfigFeature;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -11,9 +12,10 @@ public class TestBase {
     @BeforeAll
     static void setUpConfig() {
         Configuration.browserSize = "1920x1080";
-        Configuration.browser = "Chrome";
+        //Configuration.browser = "Chrome";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "user1:1234@selenoid.autotests.cloud/wd/hub";
         //пожалуйста, позвольте оставить эту строчку , хоть в каком нибудь виде
         //Configuration.holdBrowserOpen = true;
     }
